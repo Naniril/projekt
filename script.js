0456be4f5159f859b208cb6ebca269dba66c749c
Naniril <132549047+Naniril@users.noreply.github.com>
2023-05-04 13:27:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function togglemenu(){
+    let ul=document.querySelector(".aufklappMenu")
+    console.log(ul)
+}
+
+function togglemenu(){
+    // gibt popup Meldung aus
+    alart('Funktion ist gestartet')
+}
+
+console.log/('ich bin ein Konsolenausdruck'
+)
+
+document.querySelector('.box')
